412ba36dcbca376df8be3ed2ca034bb392850627
Sunny Raj <153062846+rajSunny5714@users.noreply.github.com>
2025-10-05 11:57:06 +0530

    Add files via upload

---
+++ PatternPrint/PalindromicPattern.java
@@ -0,0 +1,23 @@
+package PatternPrint;
+
+import java.util.Scanner;
+
+public class PalindromicPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
